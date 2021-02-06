5dff900e9b7ffd4f81f79653b4e6600293eb4295
Sirapat Siribandit <sirapat.siribandit@agoda.com>
2021-02-07 01:52:08 +0700

    Add http response writer method.

--- handler/http.go
+++ handler/http.go
@@ -1,5 +1,10 @@
 package handler
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type CalculatedScore struct {
 	Manager *float64 `json:"manager,omitempty"`
 	Team    *float64 `json:"team,omitempty"`
@@ -17,4 +22,33 @@ type Response struct {
 	Success bool        `json:"success"`
 	Data    interface{} `json:"data"`
 	Errors  []string    `json:"errors"`
+}
+
+func writeSuccessResponse(w http.ResponseWriter, data interface{}) {
+	resp := Response{
+		Success: true,
+		Data:    data,
+		Errors:  []string{},
+	}
+	writeResponse(w, http.StatusOK, resp)
+}
+
+func writeErrorResponse(w http.ResponseWriter, errors []error) {
+	messages := make([]string, len(errors))
+	for i, v := range errors {
+		messages[i] = v.Error()
+	}
+
+	resp := Response{
+		Success: false,
+		Data:    struct{}{},
+		Errors:  messages,
+	}
+	writeResponse(w, http.StatusBadRequest, resp)
+}
+
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(data)
 }
