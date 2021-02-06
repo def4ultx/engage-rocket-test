9f7976911a4211e2ad841a46cf4c48037d049e31
Sirapat Siribandit <sirapat.siribandit@agoda.com>
2021-02-07 01:54:55 +0700

    Refactor.

--- handler/handler.go
+++ handler/handler.go
@@ -2,7 +2,23 @@ package handler
 
 import (
 	"net/http"
+
+	"engage-rocket-test/domain"
 )
+
+type CalculatedScore struct {
+	Manager *float64 `json:"manager,omitempty"`
+	Team    *float64 `json:"team,omitempty"`
+	Others  *float64 `json:"others,omitempty"`
+}
+
+type Request struct {
+	Scores domain.Scores `json:"scores"`
+}
+
+type ResponseData struct {
+	Scores CalculatedScore `json:"scores"`
+}
 
 func CalculateScoreHandler(w http.ResponseWriter, r *http.Request) {
 }
--- handler/http.go
+++ handler/http.go
@@ -5,19 +5,6 @@ import (
 	"net/http"
 )
 
-type CalculatedScore struct {
-	Manager *float64 `json:"manager,omitempty"`
-	Team    *float64 `json:"team,omitempty"`
-	Others  *float64 `json:"others,omitempty"`
-}
-type Request struct {
-	Scores Scores `json:"scores"`
-}
-
-type ResponseData struct {
-	Scores CalculatedScore `json:"scores"`
-}
-
 type Response struct {
 	Success bool        `json:"success"`
 	Data    interface{} `json:"data"`
