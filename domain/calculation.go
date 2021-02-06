ab333e33194c5147d96aa7677e7910643578fa07
Sirapat Siribandit <sirapat.siribandit@agoda.com>
2021-02-07 01:54:27 +0700

    Change package name to domain.

--- domain/calculation.go
+++ domain/calculation.go
@@ -1,4 +1,4 @@
-package main
+package domain
 
 func AverageScore(scores []Score, minSize int) float64 {
 	if len(scores) == 0 {
--- domain/domain.go
+++ domain/domain.go
@@ -1,4 +1,4 @@
-package main
+package domain
 
 type Score struct {
 	UserId int     `json:"userId"`
